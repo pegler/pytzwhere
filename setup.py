e28f610d42757f480281cff8ca281a7baf5fdfdb
Jacinda Shelly <jshelly@doctorondemand.com>
2013-12-17 16:16:40 -0800

    Package tzwhere for PyPi

---
+++ setup.py
@@ -0,0 +1,27 @@
+import os
+from setuptools import setup
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='tzwhere',
+    version='1.0',
+    packages=['tzwhere'],
+    include_package_data=True,
+    license='MIT License',
+    description='Python library to look up timezone from lat / long offline',
+    long_description=README,
+    url='https://github.com/pegler/pytzwhere',
+    author='Matt Pegler',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Topic :: Software Development :: Localization',
+    ],
+)
