40ef000687c7bf3927b9eb7689b8a4850fa85b75
Andy Gimblett <gimbo@gimbo.org.uk>
2014-06-04 12:32:09 +0100

    flake8 cleaning
    Using the following options:
    
        [flake8]
        ignore = E123,E124,E128,E201,E202,E241
        max-line-length = 100

--- tzwhere/tzwhere.py
+++ tzwhere/tzwhere.py
@@ -16,6 +16,8 @@ class tzwhere(object):
     # By default, use the data file in our package directory
     DEFAULT_FILENAME = os.path.join(os.path.dirname(__file__),
         'tz_world_compact.json')
+    PICKLE_FILENAME = os.path.join(os.path.dirname(__file__),
+        'tz_world.pickle')
 
     def __init__(self, filename=DEFAULT_FILENAME, read_pickle=False,
             write_pickle=False):
@@ -32,11 +34,10 @@ class tzwhere(object):
         input_file.close()
 
         if write_pickle:
-            print 'Writing pickle output file: %s' % PICKLE_FILENAME
-            f = open(PICKLE_FILENAME, 'w')
+            print 'Writing pickle output file: %s' % self.PICKLE_FILENAME
+            f = open(self.PICKLE_FILENAME, 'w')
             pickle.dump(featureCollection, f, pickle.HIGHEST_PROTOCOL)
             f.close()
-
 
         self.timezoneNamesToPolygons = {}
         for feature in featureCollection['features']:
@@ -48,9 +49,11 @@ class tzwhere(object):
                     self.timezoneNamesToPolygons[tzname] = []
 
                 for raw_poly in polys:
-                    #WPS84 coordinates are [long, lat], while many conventions are [lat, long]
-                    #Our data is in WPS84.  Convert to an explicit format which geolib likes.
-                    assert len(raw_poly)%2 == 0
+                    # WPS84 coordinates are [long, lat], while many
+                    # conventions are [lat, long] Our data is in
+                    # WPS84.  Convert to an explicit format which
+                    # geolib likes.
+                    assert len(raw_poly) % 2 == 0
                     poly = []
                     while raw_poly:
                         lat = raw_poly.pop()
@@ -58,16 +61,20 @@ class tzwhere(object):
                         poly.append({'lat': lat, 'lng': lng})
                     self.timezoneNamesToPolygons[tzname].append(tuple(poly))
 
-        self.timezoneLongitudeShortcuts = {};
-        self.timezoneLatitudeShortcuts = {};
+        self.timezoneLongitudeShortcuts = {}
+        self.timezoneLatitudeShortcuts = {}
         for tzname in self.timezoneNamesToPolygons:
             for polyIndex, poly in enumerate(self.timezoneNamesToPolygons[tzname]):
                 lats = [x['lat'] for x in poly]
                 lngs = [x['lng'] for x in poly]
-                minLng = math.floor(min(lngs) / self.SHORTCUT_DEGREES_LONGITUDE) * self.SHORTCUT_DEGREES_LONGITUDE;
-                maxLng = math.floor(max(lngs) / self.SHORTCUT_DEGREES_LONGITUDE) * self.SHORTCUT_DEGREES_LONGITUDE;
-                minLat = math.floor(min(lats) / self.SHORTCUT_DEGREES_LATITUDE) * self.SHORTCUT_DEGREES_LATITUDE;
-                maxLat = math.floor(max(lats) / self.SHORTCUT_DEGREES_LATITUDE) * self.SHORTCUT_DEGREES_LATITUDE;
+                minLng = (math.floor(min(lngs) / self.SHORTCUT_DEGREES_LONGITUDE)
+                          * self.SHORTCUT_DEGREES_LONGITUDE)
+                maxLng = (math.floor(max(lngs) / self.SHORTCUT_DEGREES_LONGITUDE)
+                          * self.SHORTCUT_DEGREES_LONGITUDE)
+                minLat = (math.floor(min(lats) / self.SHORTCUT_DEGREES_LATITUDE)
+                          * self.SHORTCUT_DEGREES_LATITUDE)
+                maxLat = (math.floor(max(lats) / self.SHORTCUT_DEGREES_LATITUDE)
+                          * self.SHORTCUT_DEGREES_LATITUDE)
                 degree = minLng
                 while degree <= maxLng:
                     if degree not in self.timezoneLongitudeShortcuts:
@@ -90,55 +97,66 @@ class tzwhere(object):
                     self.timezoneLatitudeShortcuts[degree][tzname].append(polyIndex)
                     degree = degree + self.SHORTCUT_DEGREES_LATITUDE
 
-        #convert things to tuples to save memory
+        # Convert things to tuples to save memory
         for tzname in self.timezoneNamesToPolygons.keys():
-            self.timezoneNamesToPolygons[tzname] = tuple(self.timezoneNamesToPolygons[tzname])
+            self.timezoneNamesToPolygons[tzname] = \
+                tuple(self.timezoneNamesToPolygons[tzname])
         for degree in self.timezoneLatitudeShortcuts:
             for tzname in self.timezoneLatitudeShortcuts[degree].keys():
-                self.timezoneLatitudeShortcuts[degree][tzname] = tuple(self.timezoneLatitudeShortcuts[degree][tzname])
+                self.timezoneLatitudeShortcuts[degree][tzname] = \
+                    tuple(self.timezoneLatitudeShortcuts[degree][tzname])
         for degree in self.timezoneLongitudeShortcuts.keys():
             for tzname in self.timezoneLongitudeShortcuts[degree].keys():
-                self.timezoneLongitudeShortcuts[degree][tzname] = tuple(self.timezoneLongitudeShortcuts[degree][tzname])
+                self.timezoneLongitudeShortcuts[degree][tzname] = \
+                    tuple(self.timezoneLongitudeShortcuts[degree][tzname])
 
     def _point_inside_polygon(self, x, y, poly):
         n = len(poly)
-        inside =False
+        inside = False
 
         p1x, p1y = poly[0]['lng'], poly[0]['lat']
-        for i in range(n+1):
-            p2x,p2y = poly[i % n]['lng'], poly[i % n]['lat']
-            if y > min(p1y,p2y):
-                if y <= max(p1y,p2y):
-                    if x <= max(p1x,p2x):
+        for i in range(n + 1):
+            p2x, p2y = poly[i % n]['lng'], poly[i % n]['lat']
+            if y > min(p1y, p2y):
+                if y <= max(p1y, p2y):
+                    if x <= max(p1x, p2x):
                         if p1y != p2y:
-                            xinters = (y-p1y)*(p2x-p1x)/(p2y-p1y)+p1x
+                            xinters = (y - p1y) * (p2x - p1x) / (p2y - p1y) + p1x
                         if p1x == p2x or x <= xinters:
                             inside = not inside
-            p1x,p1y = p2x,p2y
+            p1x, p1y = p2x, p2y
 
         return inside
 
     def tzNameAt(self, latitude, longitude):
-        latTzOptions = self.timezoneLatitudeShortcuts[math.floor(latitude / self.SHORTCUT_DEGREES_LATITUDE) * self.SHORTCUT_DEGREES_LATITUDE]
-        latSet = set(latTzOptions.keys());
-        lngTzOptions = self.timezoneLongitudeShortcuts[math.floor(longitude / self.SHORTCUT_DEGREES_LONGITUDE) * self.SHORTCUT_DEGREES_LONGITUDE]
+        latTzOptions = self.timezoneLatitudeShortcuts[
+            (math.floor(latitude / self.SHORTCUT_DEGREES_LATITUDE)
+             * self.SHORTCUT_DEGREES_LATITUDE)
+        ]
+        latSet = set(latTzOptions.keys())
+        lngTzOptions = self.timezoneLongitudeShortcuts[
+            (math.floor(longitude / self.SHORTCUT_DEGREES_LONGITUDE)
+             * self.SHORTCUT_DEGREES_LONGITUDE)
+        ]
         lngSet = set(lngTzOptions.keys())
-        possibleTimezones = lngSet.intersection(latSet);
+        possibleTimezones = lngSet.intersection(latSet)
         if possibleTimezones:
             if False and len(possibleTimezones) == 1:
                 return possibleTimezones.pop()
             else:
                 for tzname in possibleTimezones:
-                    polyIndices = set(latTzOptions[tzname]).intersection(set(lngTzOptions[tzname]));
+                    polyIndices = set(latTzOptions[tzname]).intersection(set(lngTzOptions[tzname]))
                     for polyIndex in polyIndices:
-                        poly = self.timezoneNamesToPolygons[tzname][polyIndex];
+                        poly = self.timezoneNamesToPolygons[tzname][polyIndex]
                         if self._point_inside_polygon(longitude, latitude, poly):
                             return tzname
 
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser(description='''
-    Convert lat/lng to timezones. Specify --read_pickle to initialize from a pickle file instead of the json file.
+    Convert lat/lng to timezones.
+    Specify --read_pickle to initialize from a pickle file instead of
+    the json file.
 ''')
     parser.add_argument('--json_file', default='tz_world_compact.json',
                     help='path to the json input file')
@@ -159,9 +177,9 @@ if __name__ == "__main__":
     w = tzwhere(filename, args.read_pickle, args.write_pickle)
     end = datetime.datetime.now()
     print 'Initialized in: ',
-    print end-start
-    print w.tzNameAt(float(35.295953), float(-89.662186)) #Arlington, TN
-    print w.tzNameAt(float(33.58), float(-85.85)) #Memphis, TN
-    print w.tzNameAt(float(61.17), float(-150.02)) #Anchorage, AK
-    print w.tzNameAt(float(44.12), float(-123.22)) #Eugene, OR
-    print w.tzNameAt(float(42.652647), float(-73.756371)) #Albany, NY
+    print end - start
+    print w.tzNameAt(float(35.295953), float(-89.662186))  # Arlington, TN
+    print w.tzNameAt(float(33.58), float(-85.85))  # Memphis, TN
+    print w.tzNameAt(float(61.17), float(-150.02))  # Anchorage, AK
+    print w.tzNameAt(float(44.12), float(-123.22))  # Eugene, OR
+    print w.tzNameAt(float(42.652647), float(-73.756371))  # Albany, NY
