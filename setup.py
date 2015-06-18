d15ab04ed952cbb8adc6d1ebd52e8e08c78a1920
cstich <cstich@users.noreply.github.com>
2015-06-18 16:25:49 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='tzwhere',
-    version='1.0',
+    version='2.0',
     packages=['tzwhere'],
     package_data={
         'tzwhere': ['tz_world.json', 'tz_world_compact.json',
