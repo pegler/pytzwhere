9507ef1386f6d2c211b4887eb567cf0ea720c750
cstich <cstich@users.noreply.github.com>
2015-07-14 18:42:10 +0200

    Update getOriginalData.sh

--- getOriginalData.sh
+++ getOriginalData.sh
@@ -1,7 +1,9 @@
-# This is a basic script that downloads the original data and converts it to a JSON
+#!/bin/sh
+# Author: Christoph Stich
+# This is a basic script that download sthe original data and converts it to a JSON
 wget http://efele.net/maps/tz/world/tz_world.zip
 unzip tz_world
-ogr2ogr -f GeoJSON -t_srs crs:84 tz_world.json ./world/tz_world.shp # Doesn't play nicely with our format yet
+ogr2ogr -f GeoJSON -t_srs crs:84 tz_world.json ./world/tz_world.sph
 mv tz_world.json tzwhere/
 rm ./world/ -r
 rm tz_world.zip
