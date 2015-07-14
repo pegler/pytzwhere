9502c93e1e4de350c4da17fc89684c46c1dbdfc6
pegler <matt@goteamup.com>
2015-07-14 09:35:45 -0700

    bump version to 2.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='tzwhere',
-    version='2.0',
+    version='2.2',
     packages=['tzwhere'],
     package_data={
         'tzwhere': ['tz_world.csv']
