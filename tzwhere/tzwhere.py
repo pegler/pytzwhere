3441ca75831644597ca830e89862eb3d6d5b9d15
Ryan P. Kelly <rpkelly@cpan.org>
2016-03-04 07:51:59 -0500

    let users of the module to determine their logging configuration

--- tzwhere/tzwhere.py
+++ tzwhere/tzwhere.py
@@ -36,8 +36,6 @@ try:
 except ImportError:
     WRAP = tuple
 
-LOGGER_FORMAT = '%(asctime)-15s %(filename)s %(funcName)s %(lineno)d %(levelname)s  %(message)s'
-logging.basicConfig(format=LOGGER_FORMAT, level=logging.DEBUG)
 LOGGER = logging.getLogger('pytzwhere')
 
 
@@ -357,6 +355,10 @@ report_memory = False
 
 
 def main():
+
+    LOGGER_FORMAT = '%(asctime)-15s %(filename)s %(funcName)s %(lineno)d %(levelname)s  %(message)s'
+    logging.basicConfig(format=LOGGER_FORMAT, level=logging.DEBUG)
+
     try:
         import docopt
     except ImportError:
