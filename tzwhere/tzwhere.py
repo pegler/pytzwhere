54c017b07777b97637d80246af801651c90a603c
Christoph Stich <christoph.n.stich@gmail.com>
2016-04-24 00:44:51 +0100

    Upate the development branch

--- tzwhere/tzwhere.py
+++ tzwhere/tzwhere.py
@@ -8,35 +8,17 @@ as a script too (this requires the docopt package to be installed).
 Run it with the -h option to see usage.
 
 """
+from shapely.geometry import Polygon, Point
+from shapely.prepared import prep
 
-import csv
 import collections
 try:
     import json
 except ImportError:
     import simplejson as json
 import math
+import numpy
 import os
-import pickle
-import logging
-
-# We can speed up things by about 200 times if we use shapely
-try:
-    from shapely.geometry import Polygon, Point
-    from shapely.prepared import prep
-    SHAPELY_IMPORT = True
-except ImportError:
-    SHAPELY_IMPORT = False
-
-# We can save about 222MB of RAM by turning our polygon lists into
-# numpy arrays rather than tuples, if numpy is installed.
-try:
-    import numpy
-    WRAP = numpy.array
-except ImportError:
-    WRAP = tuple
-
-LOGGER = logging.getLogger('pytzwhere')
 
 
 class tzwhere(object):
@@ -47,8 +29,7 @@ class tzwhere(object):
     DEFAULT_JSON = os.path.join(os.path.dirname(__file__),
                                 'tz_world.json')
 
-    def __init__(self, input_kind='csv', path=None,
-                 shapely=False, forceTZ=False):
+    def __init__(self, path=None, shapely=False, forceTZ=False):
         '''
         Initializes the tzwhere class.
         @input_kind: Which filetype you want to read from
@@ -60,34 +41,19 @@ class tzwhere(object):
         bounds, you need to specify this during initialization arleady
         '''
 
-        if (shapely or forceTZ) and not SHAPELY_IMPORT:
-            raise ValueError('You need to have shapley installed for this '
-                             'feature, but we can\'t find it')
-        if not shapely and forceTZ:
-            raise ValueError('The lookup \'hack\' depends on shapely. Try using'
-                             ' shapely=True when initializing the class')
-
         self.forceTZ = forceTZ
-        self.shapely = shapely and SHAPELY_IMPORT
 
         # Construct appropriate generator for (tz, polygon) pairs.
-        if input_kind in ['json']:
-            featureCollection = tzwhere.read_tzworld(input_kind, path)
-            pgen = tzwhere._feature_collection_polygons(featureCollection)
-        else:
-            raise ValueError(input_kind)
+        featureCollection = tzwhere.read_tzworld(path)
+        pgen = tzwhere._feature_collection_polygons(featureCollection)
 
         # Turn that into an internal mapping
-        if self.shapely:
-            self._construct_shapely_map(pgen, forceTZ)
-        else:
-            self._construct_polygon_map(pgen)
+        self._construct_shapely_map(pgen, forceTZ)
 
-        # Convert polygon lists to numpy arrays or (failing that)
-        # tuples to save memory.
+        # Convert polygon lists to numpy arrays
         for tzname in self.timezoneNamesToPolygons.keys():
             self.timezoneNamesToPolygons[tzname] = \
-                WRAP(self.timezoneNamesToPolygons[tzname])
+                numpy.asarray(self.timezoneNamesToPolygons[tzname])
 
         # And construct lookup shortcuts.
         self._construct_shortcuts()
@@ -98,19 +64,11 @@ class tzwhere(object):
         self.unprepTimezoneNamesToPolygons = collections.defaultdict(list)
 
         for (tzname, poly) in polygon_generator:
-            poly = Polygon(poly)
             self.timezoneNamesToPolygons[tzname].append(
-                prep(poly))
+                poly)
             if forceTZ:
                 self.unprepTimezoneNamesToPolygons[tzname].append(
                     poly)
-
-    def _construct_polygon_map(self, polygon_generator):
-        """Turn a (tz, polygon) generator, into our internal mapping."""
-        self.timezoneNamesToPolygons = collections.defaultdict(list)
-        for (tzname, poly) in polygon_generator:
-            self.timezoneNamesToPolygons[tzname].append(
-                WRAP(poly))
 
     def _construct_shortcuts(self):
         ''' Construct our shortcuts for looking up polygons. Much faster
@@ -120,12 +78,8 @@ class tzwhere(object):
 
         for tzname in self.timezoneNamesToPolygons:
             for polyIndex, poly in enumerate(self.timezoneNamesToPolygons[tzname]):
-                if self.shapely:
-                    lngs = [x[0] for x in poly.context.exterior.coords]
-                    lats = [x[1] for x in poly.context.exterior.coords]
-                else:
-                    lngs = [x[0] for x in poly]
-                    lats = [x[1] for x in poly]
+                lngs = [x[0] for x in poly]
+                lats = [x[1] for x in poly]
                 minLng = (math.floor(min(lngs) / self.SHORTCUT_DEGREES_LONGITUDE)
                           * self.SHORTCUT_DEGREES_LONGITUDE)
                 maxLng = (math.floor(max(lngs) / self.SHORTCUT_DEGREES_LONGITUDE)
@@ -135,6 +89,7 @@ class tzwhere(object):
                 maxLat = (math.floor(max(lats) / self.SHORTCUT_DEGREES_LATITUDE)
                           * self.SHORTCUT_DEGREES_LATITUDE)
                 degree = minLng
+
                 while degree <= maxLng:
                     if degree not in self.timezoneLongitudeShortcuts:
                         self.timezoneLongitudeShortcuts[degree] = {}
@@ -176,7 +131,6 @@ class tzwhere(object):
         the closest timezone you can find instead. Only works if the point is
         reasonable close to a timezone already. Consider this a somewhat of a
         'hack'. Introduces potential errors, be warned.
-
         '''
 
         if forceTZ and not self.forceTZ:
@@ -196,20 +150,18 @@ class tzwhere(object):
         lngSet = set(lngTzOptions.keys())
         possibleTimezones = lngSet.intersection(latSet)
 
-        if self.shapely:
-            queryPoint = Point(longitude, latitude)
+        queryPoint = Point(longitude, latitude)
 
         if possibleTimezones:
             for tzname in possibleTimezones:
+                # TODO this isinstance is not implemented correctly
+                if isinstance(self.timezoneNamesToPolygons[tzname], numpy.ndarray):
+                    self.timezoneNamesToPolygons[tzname] = list(map(lambda p: prep(Polygon(p)), self.timezoneNamesToPolygons[tzname]))
                 polyIndices = set(latTzOptions[tzname]).intersection(set(lngTzOptions[tzname]))
                 for polyIndex in polyIndices:
                     poly = self.timezoneNamesToPolygons[tzname][polyIndex]
-                    if self.shapely:
-                        if poly.contains_properly(queryPoint):
-                            return tzname
-                    else:
-                        if self._point_inside_polygon(latitude,longitude, poly):
-                            return tzname
+                    if poly.contains_properly(queryPoint):
+                        return tzname
         distances = []
         if forceTZ:
             if possibleTimezones:
@@ -226,34 +178,14 @@ class tzwhere(object):
                 return sorted(distances, key=lambda x: x[1])[0][1]
 
     @staticmethod
-    def _point_inside_polygon(x, y, poly):
-        n = len(poly)
-        inside = False
-
-        p1x, p1y = poly[0][1], poly[0][0]
-        for i in range(n + 1):
-            p2x, p2y = poly[i % n][1], poly[i % n][0]
-            if y > min(p1y, p2y):
-                if y <= max(p1y, p2y):
-                    if x <= max(p1x, p2x):
-                        if p1y != p2y:
-                            xinters = (y - p1y) * (p2x - p1x) / (p2y - p1y) + p1x
-                        if p1x == p2x or x <= xinters:
-                            inside = not inside
-            p1x, p1y = p2x, p2y
-
-        return inside
-
-    @staticmethod
-    def read_tzworld(input_kind='json', path=None):
-        reader = tzwhere.read_json if input_kind == 'json' else tzwhere.read_pickle
+    def read_tzworld(path=None):
+        reader = tzwhere.read_json
         return reader(path)
 
     @staticmethod
     def read_json(path=None):
         if path is None:
             path = tzwhere.DEFAULT_JSON
-        LOGGER.info('Reading json input file: %s\n' % path)
         with open(path, 'r') as f:
             featureCollection = json.load(f)
         return featureCollection
@@ -279,7 +211,7 @@ class tzwhere(object):
 
 
 def main():
-    print('Usage of pytzwhere as a script is no longer supported. Please use version 2.3')
+    print('We have to implement a rewrite of that')
 
 if __name__ == "__main__":
     main()
