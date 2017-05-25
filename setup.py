2429aa864cac653c826c1ab39f9f88d1e8858331
Matt Pegler <pegler@gmail.com>
2017-05-25 15:37:56 -0400

    bump version to 3.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='tzwhere',
-    version='3.0a',
+    version='3.0',
     packages=['tzwhere'],
     package_data={
         'tzwhere': ['tz_world.json']
