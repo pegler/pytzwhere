a74c37c06551222011f30a9e7285f04d72d6a75b
Christopher Hawes <chrishawes@gmail.com>
2018-06-13 13:37:06 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,32 +8,31 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='tzwhere',
-    version='3.0.3',
-    packages=['tzwhere'],
+    version='0.1',
+    packages=['geodjango-tzwhere'],
     package_data={
-        'tzwhere': [
+        'geodjango-tzwhere': [
             'tz_world.json.gz',
             'tz_world_shortcuts.json'
         ]
     },
     include_package_data=True,
     install_requires=[
-        'shapely'
+        'django'
     ],
     license='MIT License',
     description='Python library to look up timezone from lat / long offline',
     long_description=README,
-    url='https://github.com/pegler/pytzwhere',
-    author='Matt Pegler',
+    url='https://github.com/cjh79/geodjango-tzwhere',
+    author='Chris Hawes',
     author_email='matt@pegler.co',
-    maintainer='Christoph Stich',
-    maintainer_email='christoph@stich.xyz',
+    maintainer='Christopher Hawes',
+    maintainer_email='chrishawes@gmail.com',
     classifiers=[
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 3',
         'Topic :: Software Development :: Localization',
     ],
